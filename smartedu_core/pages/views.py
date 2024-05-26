28e0c28480676dffaf39f6cb1198c0d15e5ca089
selcuk <yilmax4@gmail.com>
2024-05-26 17:10:39 +0300

    django MVT project first commit

---
+++ smartedu_core/pages/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+# from django.http import HttpResponse
+
+
+def index(request):
+    # return HttpResponse('<h1> INDEX SAYFASI </h1>')
+    return render(request,'index.html') 
