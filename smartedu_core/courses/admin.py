7e2d82d02a84c8d7532f52bf126a02b2061e3853
selcuk <yilmax4@gmail.com>
2024-05-28 13:39:16 +0300

    10th video included, completed and implemented

--- smartedu_core/courses/admin.py
+++ smartedu_core/courses/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from . models import Course
 
-
-admin.site.register(Course)
+@admin.register(Course)
+class CouurseAdmin(admin.ModelAdmin):
+    list_display = ('name','available')
+    list_filter = ('available',)
+    search_fields = ('name', 'description')
--- smartedu_core/courses/views.py
+++ smartedu_core/courses/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+# from django.http import HttpResponse
+from . models import Course
 
-# Create your views here.
+def course_list(request):
+    # return HttpResponse('<h1> INDEX SAYFASI </h1>')
+    courses = Course.objects.all().order_by('-date')
+
+    context = {
+        'courses' : courses
+    }
+
+
+    return render(request,'courses.html', context) 
