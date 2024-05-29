d7e1f574d4b3dbf4fe1b62b678294d5b54e8bba5
selcuk <yilmax4@gmail.com>
2024-05-29 20:58:42 +0300

    14 and 15. video lessons complated.Listview and Detailview iplemented as class based.

--- smartedu_core/coursesses/models.py
+++ smartedu_core/coursesses/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from teachers.models import Teacher
 
 
 class Category(models.Model):
@@ -17,6 +18,7 @@ class Tag(models.Model):
         
 # Create your models here.
 class Course(models.Model):
+    teacher = models.ForeignKey(Teacher, null=True, on_delete=models.CASCADE)
     name= models.CharField(max_length=200,unique=True)
     category = models.ForeignKey(Category, null=True, on_delete=models.DO_NOTHING)
     tags = models.ManyToManyField(Tag, blank=True, null=True)    
