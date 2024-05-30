8b6e4bed620165768cc3885a712118706078fb34
selcuk <yilmax4@gmail.com>
2024-05-30 16:27:03 +0300

    16.video lessan complated and search bar activated and DRY'e görecoursesses app içindeki views.py and urls.py kısaltıldı

--- smartedu_core/coursesses/views.py
+++ smartedu_core/coursesses/views.py
@@ -1,20 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # from django.http import HttpResponse
 from . models import Course, Category, Tag
 
-def course_list(request):
-    # return HttpResponse('<h1> INDEX SAYFASI </h1>')
-    courses = Course.objects.all().order_by('-date')
-    categories = Category.objects.all()
+
+def course_list(request, category_slug=None, tag_slug=None):
+    category_page = None
+    tag_page = None
+    categories = categories = Category.objects.all()
     tags = Tag.objects.all()
 
+    if category_slug != None:
+        category_page = get_object_or_404(Category, slug=category_slug)
+        courses = Course.objects.filter(available=True, category= category_page)
+
+    elif tag_slug != None:
+        tag_page = get_object_or_404(Tag, slug=tag_slug)
+        courses = Course.objects.filter(available=True, tags=tag_page)
+    else:
+        courses = Course.objects.all().order_by('-date')
+
     context = {
-        'courses' : courses,
+        'courses': courses,
         'categories': categories,
         'tags':tags
     }
 
-    return render(request,'courses.html', context) 
+    return render(request, 'courses.html', context)
+
+
 
 def course_detail(request, category_slug, course_id):
     course = Course.objects.get(category__slug=category_slug, id = course_id)
@@ -27,28 +40,57 @@ def course_detail(request, category_slug, course_id):
 
     return render(request, 'course.html', context)
 
-def category_list(request, category_slug):
-    courses = Course.objects.all().filter(category__slug= category_slug)
+
+def search(request):
+    courses = Course.objects.filter(name__contains = request.GET['search'])
     categories = Category.objects.all()
-    tags = Tag.objects.all()
+    tags= Tag.objects.all()
 
     context = {
-    'courses' : courses,
-    'categories': categories,
-    'tags':tags
-    }
-
-    return render(request, 'courses.html', context)
-
-def tag_list(request, tag_slug):
-    courses = Course.objects.all().filter(tags__slug= tag_slug)
-    categories = Category.objects.all()
-    tags = Tag.objects.all()
-
-    context = {
-        'courses' : courses,
+        'courses': courses,
         'categories': categories,
         'tags':tags
     }
 
     return render(request, 'courses.html', context)
+
+# def course_list(request):
+#     # return HttpResponse('<h1> INDEX SAYFASI </h1>')
+#     courses = Course.objects.all().order_by('-date')
+#     categories = Category.objects.all()
+#     tags = Tag.objects.all()
+
+#     context = {
+#         'courses' : courses,
+#         'categories': categories,
+#         'tags':tags
+#     }
+
+#     return render(request,'courses.html', context) 
+
+
+# def category_list(request, category_slug):
+#     courses = Course.objects.all().filter(category__slug= category_slug)
+#     categories = Category.objects.all()
+#     tags = Tag.objects.all()
+
+#     context = {
+#     'courses' : courses,
+#     'categories': categories,
+#     'tags':tags
+#     }
+
+#     return render(request, 'courses.html', context)
+
+# def tag_list(request, tag_slug):
+#     courses = Course.objects.all().filter(tags__slug= tag_slug)
+#     categories = Category.objects.all()
+#     tags = Tag.objects.all()
+
+#     context = {
+#         'courses' : courses,
+#         'categories': categories,
+#         'tags':tags
+#     }
+
+#     return render(request, 'courses.html', context)
