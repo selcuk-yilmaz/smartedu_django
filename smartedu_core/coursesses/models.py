542a06e83720cf4bc67fe2021ce54286325ce39d
selcuk <yilmax4@gmail.com>
2024-05-31 08:59:45 +0300

    video20 complated.dashboard.html created and model updated in the coursesses.py

--- smartedu_core/coursesses/models.py
+++ smartedu_core/coursesses/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from teachers.models import Teacher
-
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=40, null=True)
@@ -21,7 +21,8 @@ class Course(models.Model):
     teacher = models.ForeignKey(Teacher, null=True, on_delete=models.CASCADE)
     name= models.CharField(max_length=200,unique=True)
     category = models.ForeignKey(Category, null=True, on_delete=models.DO_NOTHING)
-    tags = models.ManyToManyField(Tag, blank=True, null=True)    
+    tags = models.ManyToManyField(Tag, blank=True, null=True)
+    students = models.ManyToManyField(User, blank=True, related_name='courses_joined')    
     description = models.TextField(blank=True, null=True)
     image = models.ImageField(upload_to="courses/%Y/%m/%d/", default="courses/default_course_image.jpg")
     date = models.DateTimeField(auto_now=True)
